a730b16eaa0e58128390ba5b8615babdd82087a0
miah002 <ulanjeremiah@yahoo.com>
2026-05-24 02:42:43 +0800

    fix: seed.ts uses Turso adapter when TURSO_* env vars set

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,20 @@ import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { computeSemiMonthlyPayroll } from "../src/lib/ph-payroll";
 
-const prisma = new PrismaClient();
+function makePrisma() {
+  if (process.env.TURSO_DATABASE_URL && process.env.TURSO_AUTH_TOKEN) {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { createClient } = require("@libsql/client");
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaLibSQL } = require("@prisma/adapter-libsql");
+    const libsql = createClient({ url: process.env.TURSO_DATABASE_URL, authToken: process.env.TURSO_AUTH_TOKEN });
+    const adapter = new PrismaLibSQL(libsql);
+    return new PrismaClient({ adapter });
+  }
+  return new PrismaClient();
+}
+
+const prisma = makePrisma();
 
 const EMPLOYEES = [
   {
